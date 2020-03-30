d06b5f56da94d9889541a238a86ef03581199667
Raza Khawaja <razamufc@hotmail.com>
2020-03-31 02:04:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from flask import Flask, request
+import json
+import urllib.request
+from fastai.vision import *
+from io import BytesIO
 
 
-
-# UPLOAD_FOLDER = '/home/raza/Desktop'
+UPLOAD_FOLDER = '/home/raza/Desktop'
 
 app = Flask(__name__)
 
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/analyze", methods = ['GET', 'POST'])
 def home():
 	if request.method == "POST":
-		image = request.files['image']
-		# print(image)
-		# filename = secure_filename(image.filename)
-		# image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		return {'result': 10}
-	else:
-		return "hroku"
+		image = request.files["file"].read()
+# print(image)
+		defaults.device = torch.device('cpu')
+		path = Path('.')
+		learner = load_learner(path, 'export.pkl')
+
+		img = open_image(BytesIO(image))
+		prediction = learner.predict(img)[0]
+
+		print(str(prediction))
+
+		return {'result': str(prediction)}
+	return "hroku"
+
 
 if __name__ == "__main__":
 	app.run(threaded = True)
