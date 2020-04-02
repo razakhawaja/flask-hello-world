528f415acc44d6b80f0e19cde79b9edca997bcca
raza <razamufc@hotmail.com>
2020-04-03 03:42:46 +0500

    Updated server and model

--- app.py
+++ app.py
@@ -13,19 +13,20 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/analyze", methods = ['GET', 'POST'])
 def home():
 	if request.method == "POST":
+		# read the image from the incoming post request
 		image = request.files["file"].read()
-# print(image)
+		# setup for pytorch to run the fastai model
 		defaults.device = torch.device('cpu')
 		path = Path('.')
+		# load the model into memory
 		learner = load_learner(path, 'export.pkl')
-
+		# Open the image in the form of bytes using BytesIO library
 		img = open_image(BytesIO(image))
+		# Predict the image and pick the prediction that has the highest probabality 
 		prediction = learner.predict(img)[0]
-
-		print(str(prediction))
-
+		# print(str(prediction))
 		return {'result': str(prediction)}
-	return "hroku"
+	return "Render Application"
 
 
 if __name__ == "__main__":
